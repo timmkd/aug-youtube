d78105225bc67e6b1623ff78cc7518d25862381b
Timothy McDonald <tim.mcdonald@deepend.melbourne>
2020-08-11 23:56:41 +1000

    index file

--- src/components/ScrollableContent.jsx
+++ src/components/ScrollableContent.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'class-names';
-import { throttle } from '../functions';
 
 class ScrollableContent extends Component {
   constructor(props) {
